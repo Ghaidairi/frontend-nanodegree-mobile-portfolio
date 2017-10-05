789b83ddb55ebf15035c2751233e65bec90665ab
Ghaidairi <Ghaidairi@hotmail.com>
2017-10-05 14:52:03 +0300

    img

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var minifyCSS = require('gulp-minify-css');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('scripts', function(){
   gulp.src('src/js/*.js')
@@ -14,5 +15,11 @@ gulp.task('styles', function(){
   .pipe(gulp.dest('dist/min-css'));
 });
 
+gulp.task('images', function(){
+	gulp.src('src/img/*.jpg')
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/img/min-img'));
+});
 
-gulp.task('default', ['scripts', 'styles']);
+
+gulp.task('default', ['scripts', 'styles', 'images']);
